c4a0fa0cf7966a83c2c9410630fa5ac2c6636ae7
Nate Olsen <NOlsen@wsecu.org>
2021-02-23 14:23:40 -0800

    suppress errors on function import

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-[string[]]$output = "`$functions_dir = `"$PSScriptRoot`""
-$output += "Get-ChildItem -Path `$functions_dir -File -Filter *ps1  | %{ Import-Module `$_.FullName }"
+[string[]]$output = "`n`$functions_dir = `"$PSScriptRoot`""
+$output += "Get-ChildItem -Path `$functions_dir -File -Filter *ps1  | %{ Import-Module `$_.FullName -ErrorAction SilentlyContinue}"
 $output |
     Out-File -FilePath $PROFILE -Append -NoClobber -Encoding ascii
