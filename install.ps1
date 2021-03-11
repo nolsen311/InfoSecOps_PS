4a8926cbf12075b199f75fcb7a68dbc6e132da3b
Nate Olsen <NOlsen@wsecu.org>
2021-03-11 19:44:00 +0000

    Updated install.ps1 - newline included to prevent breakage

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-[string[]]$output = "`$functions_dir = `"$PSScriptRoot`""
+[string[]]$output = "`n`$functions_dir = `"$PSScriptRoot`""
 $output += "Get-ChildItem -Path `$functions_dir -File -Filter *ps1  | %{ Import-Module `$_.FullName }"
 if (Test-Path $PROFILE -PathType Leaf) {
     $output |
