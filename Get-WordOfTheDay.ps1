77898c6082882f296cba90ed25237e9bec69acbd
Nathan Olsen <nolsen311@gmail.com>
2023-05-04 10:07:16 -0700

    Delete Get-WordOfTheDay.ps1

--- Get-WordOfTheDay.ps1
+++
@@ -1,6 +0,0 @@
-#Requires -PSEdition Desktop
-Function Get-WordOfTheDay {
-    $ie = Invoke-WebRequest -Uri "http://febe/teamsites/it/wotd.aspx" -UseDefaultCredentials
-    $word_of_the_day = $ie.ParsedHtml.GetElementById("WOTD").InnerText
-    Write-Host $word_of_the_day -ForegroundColor Green
-}
